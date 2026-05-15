a69d26f88e4e644d8fae003735579918e652cbe2
Edwin Henao <edwinandreshenao60@gmail.com>
2026-05-15 16:39:30 -0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  standalone: false,
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'Taller_Cambio-Dinamico-de-colores-en-Angular_15-05-2026';
+}
