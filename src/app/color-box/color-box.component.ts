1769a0f0e8a13d95b9b3a42ea9cf76688255277a
Edwin Henao <edwinandreshenao60@gmail.com>
2026-05-15 10:15:17 -0500

    feat(color-box): configurar decorador @Input y maquetar el HTML para recibir colores

--- src/app/color-box/color-box.component.ts
+++ src/app/color-box/color-box.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-color-box',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './color-box.component.scss'
 })
 export class ColorBoxComponent {
-
+  @Input() receivedColor: string = 'White';
 }
